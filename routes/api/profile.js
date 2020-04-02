cc1a4fc6b1290cdfc2569ff35c12786fdaccdd89
Samuel Seda <zyxsamys@gmail.com>
2020-04-02 14:59:21 -0600

    delete user with profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -141,4 +141,23 @@ router.get('/user/:id', async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user & posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove user posts
+
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Error server');
+  }
+});
+
 module.exports = router;
