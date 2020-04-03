d0a8607b03fef732b1c85462bc04d217a4fba678
Samuel Seda <zyxsamys@gmail.com>
2020-04-03 17:49:11 -0600

    delete post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -78,4 +78,30 @@ router.get('/:id', auth, async (req, res) => {
   }
 });
 
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    if (post.user.toString() != req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+    await post.remove();
+
+    res.json('Post deleted');
+  } catch (err) {
+    console.error(err.message);
+    if (err.message.includes('ObjectId')) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
