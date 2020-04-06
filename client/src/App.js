339af84b7086cd17106781fbec8413cda1d57d06
Samuel Seda <zyxsamys@gmail.com>
2020-04-05 20:42:35 -0500

    Custom route component privateroute

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,10 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 import Alert from './components/layout/Alert';
 import './App.css';
+import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -31,6 +33,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
